db81afc8df39dbd994823a12627ebe0a69b6f25b
soffarciniegas17 <sofiarciniegas17@gmail.com>
2018-08-01 08:47:01 -0500

    aver

--- app/src/main/java/com/sophia1/turismo_app/Splash.java
+++ app/src/main/java/com/sophia1/turismo_app/Splash.java
@@ -1,5 +1,7 @@
 package com.sophia1.turismo_app;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,13 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, Home.class);
+                startActivity(i);
+                finish();
+            }
+        }, 4000);
     }
 }
