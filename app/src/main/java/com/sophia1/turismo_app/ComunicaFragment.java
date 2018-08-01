daa2c2798a567968e0e3eef389153242cf67296c
axel8526 <casanova8526@gmail.com>
2018-08-01 11:23:01 -0500

    version Alex

---
+++ app/src/main/java/com/sophia1/turismo_app/ComunicaFragment.java
@@ -0,0 +1,6 @@
+package com.sophia1.turismo_app;
+
+public interface ComunicaFragment  {
+
+    abstract void itemPresionado(int item);
+}
--- app/src/main/java/com/sophia1/turismo_app/Home.java
+++ app/src/main/java/com/sophia1/turismo_app/Home.java
@@ -1,6 +1,9 @@
 package com.sophia1.turismo_app;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,7 +17,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class Home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, ComunicaFragment {
+
+    private int fragmentActivo, categoria, itemPress;
+    private boolean visualizacion;
+    private FloatingActionButton fab;
+    private MenuItem actionVisualizacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +31,8 @@ public class Home extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+         fab = (FloatingActionButton) findViewById(R.id.fab);
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -47,7 +49,10 @@ public class Home extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        } else if(fragmentActivo==3) {
+
+            abreLista();
+        }else{
             super.onBackPressed();
         }
     }
@@ -55,6 +60,29 @@ public class Home extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+
+        actionVisualizacion=menu.findItem(R.id.action_visualizacion);
+        if (visualizacion){
+            actionVisualizacion.setIcon(R.drawable.view_list);
+        }else{
+            actionVisualizacion.setIcon(R.drawable.view_grid);
+
+        }
+
+        //verificar la orientacion para poner invisible el icono
+        int orientation=this.getResources().getConfiguration().orientation;
+        if(orientation!= Configuration.ORIENTATION_PORTRAIT){
+            actionVisualizacion.setVisible(false);
+        }
+        //se inicia los fragment desde el metodo del menu para evitar errores del actionVisualizacion null
+        if(fragmentActivo==1){
+            abreInicio();
+        }else if(fragmentActivo==2){
+            abreLista();
+        }else{
+            itemPresionado(itemPress);
+        }
+
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
@@ -67,7 +95,26 @@ public class Home extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+
+
+        if (id == R.id.action_exit) {
+
+
+            return true;
+        }else if(id==R.id.action_visualizacion){
+
+            if(visualizacion){
+                visualizacion=false;
+                actionVisualizacion.setIcon(R.drawable.view_list);
+                abreLista();
+            }else{
+                visualizacion=true;
+                actionVisualizacion.setIcon(R.drawable.view_grid);
+                abreLista();
+
+            }
+
+
             return true;
         }
 
@@ -80,22 +127,93 @@ public class Home extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.bton_inicio) {
+            abreInicio();
+        } else if (id == R.id.bton_lugar) {
+            categoria=0;
+            abreLista();
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.bton_hotel) {
+            categoria=1;
+            abreLista();
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.bton_restaurante) {
+            categoria=2;
+            abreLista();
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void abreInicio(){
+
+        fragmentActivo=1;
+        actionVisualizacion.setVisible(false);
+
+    }
+    public void abreLista(){
+
+
+
+
+        fragmentActivo=2;
+        actionVisualizacion.setVisible(true);
+        if (categoria==0)actionVisualizacion.setIcon(R.drawable.icon_places);
+        else if (categoria==1)actionVisualizacion.setIcon(R.drawable.icon_hotel);
+        else if (categoria==2)actionVisualizacion.setIcon(R.drawable.icon_restaurant);
+    }
+
+
+    @Override
+    public void itemPresionado(int item) {
+
+
+
+        itemPress=item;
+        fragmentActivo=3;
+        actionVisualizacion.setVisible(true);
+        actionVisualizacion.setIcon(R.drawable.icon_car);
+
+    }
+    public void onResume(){
+        super.onResume();
+        SharedPreferences datos= PreferenceManager.getDefaultSharedPreferences(this);
+
+        visualizacion=datos.getBoolean("VISUALIZACION",true);
+        fragmentActivo=datos.getInt("FRAGMENT_ACTIVO",1);
+        categoria=datos.getInt("CATEGORIA",0);
+        itemPress=datos.getInt("ITEM_PRESIONADO",0);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               //abrir mapa
+                if(fragmentActivo==2){
+
+                }else if(fragmentActivo==3){
+
+                }
+            }
+        });
+
+
+
+    }
+    public void onPause(){
+        super.onPause();
+
+        SharedPreferences datos= PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor guarda=datos.edit();
+
+        guarda.putBoolean("VISUALIZACION",visualizacion);
+        guarda.putInt("FRAGMENT_ACTIVO",fragmentActivo);
+        guarda.putInt("CATEGORIA",categoria);
+        guarda.putInt("ITEM_PRESIONADO",itemPress);
+
+
+        guarda.apply();
     }
 }
