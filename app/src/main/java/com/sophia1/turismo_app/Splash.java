3d129c12fc421be834c2722af78605b0c4043795
soffarciniegas17 <sofiarciniegas17@gmail.com>
2018-08-01 08:12:25 -0500

    Initial commit

---
+++ app/src/main/java/com/sophia1/turismo_app/Splash.java
@@ -0,0 +1,15 @@
+package com.sophia1.turismo_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
