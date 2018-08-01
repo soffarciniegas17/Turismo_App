b8ea18365b2a771647d7003b7c5a5e631ebb5a14
soffarciniegas17 <sofiarciniegas17@gmail.com>
2018-08-01 09:57:42 -0500

    jhjl

---
+++ app/src/main/java/com/sophia1/turismo_app/Inicio_Frag.java
@@ -0,0 +1,29 @@
+package com.sophia1.turismo_app;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Inicio_Frag extends Fragment {
+
+
+    public Inicio_Frag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio_, container, false);
+    }
+
+}
